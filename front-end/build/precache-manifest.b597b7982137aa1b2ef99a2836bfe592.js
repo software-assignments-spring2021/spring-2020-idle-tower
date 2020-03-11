e763943e7ce088566d830bbfb3f9ca4b5c2a4330
thericfang <ericfang11@gmail.com>
2020-03-11 17:10:03 -0400

    node.js project create

---
+++ front-end/build/precache-manifest.b597b7982137aa1b2ef99a2836bfe592.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df2fea75cb5d4291e4d46de722ff21a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3781f0454523a38251d2",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6e17a07bf2919d3a414e",
+    "url": "/static/js/2.086f3c89.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.086f3c89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3781f0454523a38251d2",
+    "url": "/static/js/main.72af1596.chunk.js"
+  },
+  {
+    "revision": "673d8ffee3e8ed730e63",
+    "url": "/static/js/runtime-main.a0a66673.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
