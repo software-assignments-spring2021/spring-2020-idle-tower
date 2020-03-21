9bed448d1969c8406844ba6ff671a2222b9aba9e
Jake Berkson <berksonj@gmail.com>
2020-03-21 15:02:11 -0400

    Add tests

--- front-end/src/App.test.js
+++ front-end/src/App.test.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import { act } from 'react-dom/test-utils';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+let container;
+
+beforeEach(() => {
+	container = document.createElement('div');
+	document.body.appendChild(container);
+});
+
+afterEach(() => {
+	document.body.removeChild(container);
+	container = null;
+});
+
+
+test('checks for FoodCheck Header', () => {
+	act(() => {
+		ReactDOM.render(<App />, container);
+	});
+
+	const h1 = container.querySelector('h1');
+
+	expect(h1.textContent).toBe('FoodCheck Header');
+});
+
+test('checks for nav items', () => {
+	act(() => {
+		ReactDOM.render(<App />, container);
+	});
+
+	const nav = container.querySelector('nav');
+
+	expect(nav.textContent).toContain('Home');
+	expect(nav.textContent).toContain('Example');
+	expect(nav.textContent).not.toContain('This should not be in the nav');
 });
