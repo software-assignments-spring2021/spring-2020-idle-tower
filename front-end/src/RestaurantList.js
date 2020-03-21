a8047eec2bbab292b04cd231b0dcbec5a8af2267
Jake Berkson <berksonj@gmail.com>
2020-03-21 14:07:52 -0400

    Start RestaurantList component

---
+++ front-end/src/RestaurantList.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import './RestaurantList.scss';
+
+
+
+const InfoBox = (props) => {
+	const id = props.id
+
+	const [data, setData] = useState(0);
+
+	// const response = await axios(
+	// 	"https://my.api.mockaroo.com/business.json?key=71f47770"
+	// );
+	// setData(response)
+
+	fetch('https://my.api.mockaroo.com/business.json?key=71f47770')
+		.then(response => response.json())
+		.then(d => {
+			console.log(d);
+			// setData([d]);
+
+			// if (d['error']) {
+			// 	// handle mockaroo API error
+			// }
+		})
+
+    return (
+        <div className="InfoBox" key={id}>
+            {data}
+        </div>
+    )
+}
+
+
+
+const RestaurantList = (props) => {
+	const rlist = props.restaurant_ids || [1,2,3];
+	const rows = []
+	for(const r of rlist) {
+		rows.push(<InfoBox id={r} key={r} />)
+	}
+
+    return (
+        <div className="RestaurantList">
+        	{rows}
+        </div>
+    )
+}
+
+export default RestaurantList;
