2593a475347e82f15cb7c996bfa32d8daf933e26
Jake Berkson <berksonj@gmail.com>
2020-03-26 11:08:21 -0400

    Add login component and styling

---
+++ front-end/src/login.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Login.scss';
+
+import Notification from './Notification.js';
+
+
+const Login = (props) => {
+    return (
+        <div className="Login">
+            <Notification text={props.notificationText} error={props.error} />
+
+            <form action="" method="" acceptCharset="utf-8">
+				<label>Username: <input type="text" name="username" /></label>
+				<label>Password: <input type="password" name="password" /></label>
+				<input type="submit" name="submit" value="Submit" />
+			</form>
+        </div>
+    )
+}
+
+export default Login;
