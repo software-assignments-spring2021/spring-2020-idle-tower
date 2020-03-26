c4f440c4eabdd66e086d2d1d5172f9edd076d8dc
Eric Fang <ericfang11@gmail.com>
2020-03-26 10:06:09 -0700

    Decapitlized filenames for imports, Prettify

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,45 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.scss";
 
-import {
-	BrowserRouter as Router,
-	Switch,
-	Route,
-	Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 // Components
-import Login from './Login.js';
-
+import Login from "./login.js";
 
 export default function App() {
-	return (
-		<Router>
-			<div className="container">
-				<header>
-					<h1>FoodCheck Header</h1>
-				</header>
-				<nav>
-					<ul>
-						<li>
-							<Link to="/">Home</Link>
-						</li>
-						<li>
-							<Link to="/login">Login</Link>
-						</li>
-					</ul>
-				</nav>
+  return (
+    <Router>
+      <div className="container">
+        <header>
+          <h1>FoodCheck Header</h1>
+        </header>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </nav>
 
-				<Switch>
-					<Route exact path="/">
-						<div>Home</div>
-					</Route>
-					<Route path="/login">
-						<Login notificationText="Welcome to FoodCheck! Please login" error="Invalid credentials" />
-					</Route>
-				</Switch>
-			</div>
-		</Router>
-	);
+        <Switch>
+          <Route exact path="/">
+            <div>Home</div>
+          </Route>
+          <Route path="/login">
+            <Login
+              notificationText="Welcome to FoodCheck! Please login"
+              error="Invalid credentials"
+            />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
--- front-end/src/login.js
+++ front-end/src/login.js
@@ -1,21 +1,24 @@
-import React from 'react';
-import './Login.scss';
+import React from "react";
+import "./login.scss";
 
-import Notification from './Notification.js';
+import Notification from "./notification.js";
 
+const Login = props => {
+  return (
+    <div className="Login">
+      <Notification text={props.notificationText} error={props.error} />
 
-const Login = (props) => {
-    return (
-        <div className="Login">
-            <Notification text={props.notificationText} error={props.error} />
-
-            <form action="" method="" acceptCharset="utf-8">
-				<label>Username: <input type="text" name="username" /></label>
-				<label>Password: <input type="password" name="password" /></label>
-				<input type="submit" name="submit" value="Submit" />
-			</form>
-        </div>
-    )
-}
+      <form action="" method="" acceptCharset="utf-8">
+        <label>
+          Username: <input type="text" name="username" />
+        </label>
+        <label>
+          Password: <input type="password" name="password" />
+        </label>
+        <input type="submit" name="submit" value="Submit" />
+      </form>
+    </div>
+  );
+};
 
 export default Login;
