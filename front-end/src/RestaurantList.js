f1d467114f931766f4e4cbc0e84797dbd0d70299
Jake Berkson <berksonj@gmail.com>
2020-03-30 12:23:03 -0400

    Update styles

--- front-end/src/RestaurantList.js
+++ front-end/src/RestaurantList.js
@@ -5,8 +5,10 @@ import InfoBox from './InfoBox.js';
 
 const RestaurantList = (props) => {
 	const data = props.response;
+	const empty_obj = {'name': 'Loading', 'address': 'Loading', 'review_count': 0};
+	const empty_infobox = <InfoBox key='empty' data={empty_obj} number='1' />
 
-	const [rows, setRows] = useState([]);
+	const [rows, setRows] = useState([empty_infobox]);
 	useEffect(() => {
 		if (data !== 0) {
 			const tempRows = []
