1a7535503671aab6c2cf5bce501e81f635face42
Eric Fang <ericfang11@gmail.com>
2020-04-08 13:03:39 -0700

    added hamburger menu

---
+++ front-end/src/sidebar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { slide as Menu } from "react-burger-menu";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+export default (props) => {
+  return (
+    // Pass on our props
+    <Menu {...props}>
+      <Link to="/" className="menu-item">
+        Home
+      </Link>
+      <Link to="/login" className="menu-item">
+        Login
+      </Link>
+      <Link to="/signup" className="menu-item">
+        Sign Up
+      </Link>
+      <Link to="/market-research" className="menu-item">
+        Market Research
+      </Link>
+    </Menu>
+  );
+};
