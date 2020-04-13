f778ff25b193d9401033d645e102bb2653bf5864
Jake Berkson <berksonj@gmail.com>
2020-04-13 12:23:05 -0400

    Add login mock data

--- front-end/src/login-context.js
+++ front-end/src/login-context.js
@@ -1,14 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
+
 
 const LoginContext = React.createContext([false, () => {}]);
 
 const LoginProvider = (props) => {
-  const [loggedIn, setLoggedIn] = useState(false);
-  return (
-    <LoginContext.Provider value={[loggedIn, setLoggedIn]}>
-      {props.children}
-    </LoginContext.Provider>
-  );
+	const [loggedIn, setLoggedIn] = useState(false);
+
+	// Get mock data
+	useEffect(() => {
+		fetch('https://my.api.mockaroo.com/login.json?key=71f47770')
+		.then(response => response.json())
+		.then(d => {
+			setLoggedIn(d['valid']);
+		});
+	}, [])
+
+	return (
+		<LoginContext.Provider value={[loggedIn, setLoggedIn]}>
+			{props.children}
+		</LoginContext.Provider>
+	);
 }
 
 export { LoginContext, LoginProvider };
+
+
+
+
