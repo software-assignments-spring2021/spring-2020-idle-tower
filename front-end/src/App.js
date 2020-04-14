fb309e6a994b1807ee80788c5f0ae4848e817974
Jake Berkson <berksonj@gmail.com>
2020-04-14 14:06:51 -0400

    fix filename errors

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./lib/reset.css";
-import "./App.scss";
+import "./app.scss";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 // Components
 import Login from "./login.js";
