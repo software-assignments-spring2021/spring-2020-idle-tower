4034de6bff964734a5670b2c2262744d1d3a6895
Jake Berkson <berksonj@gmail.com>
2020-04-18 16:11:57 -0400

    Add conditional menu items

--- front-end/src/sidebar.js
+++ front-end/src/sidebar.js
@@ -1,23 +1,43 @@
-import React from "react";
+import React, { useContext } from "react";
 import { slide as Menu } from "react-burger-menu";
 import { Link } from "react-router-dom";
+import { UserContext } from './user-context.js'
 
 export default (props) => {
+  const [userState] = useContext(UserContext);
+  let login_link = null;
+  let signup_link = null;
+  let logout_link = null;
+  if (!userState['logged-in']) {
+    login_link = (
+      <Link to="/login" className="menu-item">
+        Login
+      </Link>
+    )
+    signup_link = (
+      <Link to="/signup" className="menu-item">
+        Sign Up
+      </Link>
+    )
+  } else {
+    logout_link = (
+      <Link to="/logout" className="menu-item">
+        Logout
+      </Link>
+    )
+  }
   return (
     // Pass on our props
     <Menu {...props}>
       <Link to="/" className="menu-item">
         Home
       </Link>
-      <Link to="/login" className="menu-item">
-        Login
-      </Link>
-      <Link to="/signup" className="menu-item">
-        Sign Up
-      </Link>
+      { login_link }
+      { signup_link }
       <Link to="/market-research" className="menu-item">
         Market Research
       </Link>
+      { logout_link }
     </Menu>
   );
 };
