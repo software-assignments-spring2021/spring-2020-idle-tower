caed245bf8ad6feec7d80243cb2b3b41358cb35c
Jake Berkson <berksonj@gmail.com>
2020-04-19 11:47:00 -0400

    eslint fixes

--- back-end/routes/user.js
+++ back-end/routes/user.js
@@ -1,38 +1,38 @@
 const express = require('express');
+
 const router = express.Router();
 const axios = require('axios');
 
-function sendData(res, restaurant_id, error) {
-  const data = {'error': error};
+function sendData(res, restaurantId, error) {
+  const data = { error };
   if (error) {
-    data['message'] = `Could not save restaurant ${restaurant_id} to user's saved_restaurants list.`
+    data.message = `Could not save restaurant ${restaurantId} to user's saved_restaurants list.`;
   } else {
-    data['message'] = `Successfully saved restaurant ${restaurant_id} to user's saved_restaurants list.`
+    data.message = `Successfully saved restaurant ${restaurantId} to user's saved_restaurants list.`;
   }
   res.json(data);
 }
 
 // POST /save-restaurant
 router.post('/save-restaurant', (req, res) => {
-  const restaurant_id = req.body.restaurant_id;
+  const { restaurantId } = req.body;
 
-  // TODO: save restaurant_id to user's saved_restaurants list
+  // TODO: save restaurantId to user's saved_restaurants list
   // req.user.markModified('saved_restaurants');
   // req.user.save(function(err, modifiedUser) {
   //  console.log(err, modifiedUser);
   // });
 
   axios.post('https://my.api.mockaroo.com/user_save-restaurant.json?key=71f47770', {
-    restaurant_id: restaurant_id
+    restaurantId,
   })
-  .then(function (response) {
-    console.log('response', response.data['error']);
-    const err = response.data['error'];
-    sendData(res, restaurant_id, err);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+    .then((response) => {
+      const err = response.data.error;
+      sendData(res, restaurantId, err);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 });
 
 
