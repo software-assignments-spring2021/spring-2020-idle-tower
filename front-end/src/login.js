7e7d5ec016b2d27d2d8200606b8847efb73fe513
Jake Berkson <berksonj@gmail.com>
2020-04-19 10:44:42 -0400

    eslint src/login.js --fix

--- front-end/src/login.js
+++ front-end/src/login.js
@@ -1,24 +1,26 @@
-import React from "react";
-import "./login.scss";
+import React from 'react';
+import './login.scss';
 
-import Notification from "./notification.js";
+import Notification from './notification.js';
 
-const Login = props => {
-  return (
-    <div className="Login">
-      <Notification text={props.notificationText} error={props.error} />
-      <h1>Testing</h1>
-      <form action="" method="" acceptCharset="utf-8">
-        <label>
-          Username: <input type="text" name="username" />
-        </label>
-        <label>
-          Password: <input type="password" name="password" />
-        </label>
-        <input type="submit" name="submit" value="Submit" />
-      </form>
-    </div>
-  );
-};
+const Login = (props) => (
+  <div className="Login">
+    <Notification text={props.notificationText} error={props.error} />
+    <h1>Testing</h1>
+    <form action="" method="" acceptCharset="utf-8">
+      <label>
+        Username:
+        {' '}
+        <input type="text" name="username" />
+      </label>
+      <label>
+        Password:
+        {' '}
+        <input type="password" name="password" />
+      </label>
+      <input type="submit" name="submit" value="Submit" />
+    </form>
+  </div>
+);
 
 export default Login;
