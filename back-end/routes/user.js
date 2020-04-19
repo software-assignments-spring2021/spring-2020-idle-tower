1d1b019581eec44d37205bb813db60f08b744428
Jake Berkson <berksonj@gmail.com>
2020-04-19 11:39:24 -0400

    get data from mockaroo

--- back-end/routes/user.js
+++ back-end/routes/user.js
@@ -1,26 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
+
+function sendData(res, restaurant_id, error) {
+  const data = {'error': error};
+  if (error) {
+    data['message'] = `Could not save restaurant ${restaurant_id} to user's saved_restaurants list.`
+  } else {
+    data['message'] = `Successfully saved restaurant ${restaurant_id} to user's saved_restaurants list.`
+  }
+  res.json(data);
+}
 
 // POST /save-restaurant
 router.post('/save-restaurant', (req, res) => {
-	const restaurant_id = req.body.restaurant_id;
-	console.log(req.body);
+  const restaurant_id = req.body.restaurant_id;
 
-	// TODO: save restaurant_id to user's saved_restaurants list
-	// req.user.markModified('saved_restaurants');
-	// req.user.save(function(err, modifiedUser) {
-	// 	console.log(err, modifiedUser);
-	// });
+  // TODO: save restaurant_id to user's saved_restaurants list
+  // req.user.markModified('saved_restaurants');
+  // req.user.save(function(err, modifiedUser) {
+  //  console.log(err, modifiedUser);
+  // });
 
-	const err = false;
-	let data = {'error': false};
-	if (err) {
-		data['error'] = true;
-		data['message'] = "Could not save restaurant to user's saved_restaurants list."
-	} else {
-		data['message'] = `Successfully saved restaurant ${restaurant_id} to user's saved_restaurants list.`
-	}
-	res.json(data);
+  axios.post('https://my.api.mockaroo.com/user_save-restaurant.json?key=71f47770', {
+    restaurant_id: restaurant_id
+  })
+  .then(function (response) {
+    console.log('response', response.data['error']);
+    const err = response.data['error'];
+    sendData(res, restaurant_id, err);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 });
+
 
 module.exports = router;
