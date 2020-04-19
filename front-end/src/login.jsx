afd8d9913bb271a26ad2b0cbcd566c10ef1b32a9
Jake Berkson <berksonj@gmail.com>
2020-04-19 11:04:54 -0400

    Fix login.jsx eslint warnings

--- front-end/src/login.jsx
+++ front-end/src/login.jsx
@@ -1,26 +1,41 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './login.scss';
 
 import Notification from './notification';
 
-const Login = (props) => (
-  <div className="Login">
-    <Notification text={props.notificationText} error={props.error} />
-    <h1>Testing</h1>
-    <form action="" method="" acceptCharset="utf-8">
-      <label>
-        Username:
-        {' '}
-        <input type="text" name="username" />
-      </label>
-      <label>
-        Password:
-        {' '}
-        <input type="password" name="password" />
-      </label>
-      <input type="submit" name="submit" value="Submit" />
-    </form>
-  </div>
-);
+const Login = (props) => {
+  const { notificationText, error } = props;
+
+  return (
+    <div className="Login">
+      <Notification text={notificationText} error={error} />
+      <h1>Testing</h1>
+      <form action="" method="" acceptCharset="utf-8">
+        <label htmlFor="username">
+          Username:
+          {' '}
+          <input type="text" name="username" id="username" />
+        </label>
+        <label htmlFor="password">
+          Password:
+          {' '}
+          <input type="password" name="password" id="password" />
+        </label>
+        <input type="submit" name="submit" value="Submit" />
+      </form>
+    </div>
+  );
+};
+
+Login.propTypes = {
+  notificationText: PropTypes.string,
+  error: PropTypes.string,
+};
+
+Login.defaultProps = {
+  notificationText: 'Welcome to FoodCheck! Please login',
+  error: '',
+};
 
 export default Login;
