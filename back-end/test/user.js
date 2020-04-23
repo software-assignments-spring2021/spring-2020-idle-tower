421facbf7b09d20a25fffe0060e1b9e2e99178cc
Jake Berkson <berksonj@gmail.com>
2020-04-23 18:37:47 -0400

    eslint test/user.js

--- back-end/test/user.js
+++ back-end/test/user.js
@@ -1,22 +1,21 @@
-const chai = require('chai');  
-const assert = chai.assert;
-const should = chai.should();
-const app = require("../app");
-const chaiHttp = require("chai-http");
+const chai = require('chai');
+
+const { assert } = chai;
+const chaiHttp = require('chai-http');
+const app = require('../app');
+
 chai.use(chaiHttp);
 
 
-
-describe('POST /save-restaurant route', function() {
-  it('should respond with JSON data = {error, message}', function() {
-    chai.request(app).
-      post('/user/save-restaurant/')
-      .end((err,res)=>{
+describe('POST /save-restaurant route', () => {
+  it('should respond with JSON data = {error, message}', () => {
+    chai.request(app)
+      .post('/user/save-restaurant/')
+      .end((err, res) => {
         assert.equal(res.status, 200);
         // console.log("Response Body:", res.body);
         assert.exists(res.body.error);
         assert.exists(res.body.message);
-      })
-
+      });
   });
 });
