9342424e0d1a564569d0fc2c34ce7bc175bea415
Jake Berkson <berksonj@gmail.com>
2020-04-23 18:32:45 -0400

    Add test for POST /save-restaurant route

--- back-end/test/user.js
+++ back-end/test/user.js
@@ -1,3 +1,22 @@
-var chai = require('chai');  
-var assert = chai.assert;
-var should = chai.should();
+const chai = require('chai');  
+const assert = chai.assert;
+const should = chai.should();
+const app = require("../app");
+const chaiHttp = require("chai-http");
+chai.use(chaiHttp);
+
+
+
+describe('POST /save-restaurant route', function() {
+  it('should respond with JSON data = {error, message}', function() {
+    chai.request(app).
+      post('/user/save-restaurant/')
+      .end((err,res)=>{
+        assert.equal(res.status, 200);
+        // console.log("Response Body:", res.body);
+        assert.exists(res.body.error);
+        assert.exists(res.body.message);
+      })
+
+  });
+});
