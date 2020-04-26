20ac0956d97ac3a00ab7a424d1f96fdd3fb201d1
Jake Berkson <berksonj@gmail.com>
2020-04-26 15:17:47 -0400

    Setup mongoose and passport

--- back-end/app.js
+++ back-end/app.js
@@ -1,4 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const db = require('./db');
+
+
+// connect to db
+mongoose.connect(db.dbconf, db.config);
+
+
+
 const app = express();
 module.exports = app;
 
@@ -8,6 +17,7 @@ const multer = require("multer"); // handle HTTP POST with file uploads
 const axios = require("axios"); // making request to API
 const morgan = require("morgan"); // logging HTTP requests
 const cors = require('cors'); // allow Cross Site Requests
+const passport = require('passport'); // auth
 
 // Import constants
 const CONSTANTS = require("./_constants");
@@ -24,6 +34,50 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
+
+
+// get config
+let secret;
+let port;
+let url;
+if (process.env.NODE_ENV === 'PRODUCTION') {
+	// if we're in PRODUCTION mode, then read the configration from a file
+	// use blocking file io to do this...
+	const fs = require('fs');
+	const path = require('path');
+	const fn = path.join(__dirname, '../config.json');
+	const data = fs.readFileSync(fn);
+
+	// our configuration file will be in json, so parse it and set the
+	// conenction string appropriately!
+	const conf = JSON.parse(data);
+	secret = conf.sessionSecret;
+	port = conf.port;
+	url = conf.url;
+} else {
+	// if we're not in PRODUCTION mode, then use
+	secret = '12345678910';
+	port = 3000;
+	url = 'http://localhost';
+}
+
+// express-session middleware
+// creates sessions for users
+const sessionOptions = { 
+	secret: secret, 
+	saveUninitialized: false, 
+	resave: false 
+};
+app.use(session(sessionOptions));
+
+// Setup Passport
+// https://code.tutsplus.com/tutorials/authenticating-nodejs-applications-with-passport--cms-21619
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+const initPassport = require('./passport/init');
+initPassport(passport);
 
 
 // Routes
