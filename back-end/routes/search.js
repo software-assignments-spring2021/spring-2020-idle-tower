bbe376cc08eca51ff5be19394ac6939ac287d322
Melody Lam <yuetyumelody@gmail.com>
2020-04-26 12:34:17 -0400

    added search function

--- back-end/routes/search.js
+++ back-end/routes/search.js
@@ -3,18 +3,43 @@ const router = require('express').router();
 const axios = require("axios");
 
 
-router.get("/", function(req,res){
-    axios.get("https://my.api.mockaroo.com/business.json?key=48c08110")
-    
-    if (req.query.search){
-        //get relevant search results
-        const regex = new RegExp(escapeRegex(req.query.search), 'gi');
-        // get results from db 
+//mockaroo data object
+const searchData = () => {
+    try {
+        return axios.get('https://my.api.mockaroo.com/business.json?key=48c08110')
+    } catch (error) {
+        console.log(error);
 
-
-    } else {
-        //get top restaurants around
     }
+}
+let searchResult = [];
+
+const category = searchData.category;
+
+
+router.get("/search", function(req,res){
+
+
+    for (let i = 0; i<searchData.length; i++){
+        for (let j=0; j<searchData[i].category.length; j++){
+            if (searchData[i].category[j] === req.param.id){
+                if(! searchResult.include(searchData[i]) ){
+                    searchResult.push(searchData[i]);
+                }
+            }
+        }
+    }
+    // if (req.query.search){
+    //     //get relevant search results
+    //     const regex = new RegExp(escapeRegex(req.query.search), 'gi');
+    //     // get results from db 
+    //     res.send()
+
+
+    // } else {
+    //     //get top restaurants around
+
+    // }
 
 });
 
