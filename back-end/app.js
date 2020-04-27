fc96cedcb96d554fd864dd639d0ed6473fc0e605
Eric Fang <ericfang11@gmail.com>
2020-04-27 16:27:49 -0700

    Reallign spacing and serve static files

--- back-end/app.js
+++ back-end/app.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const db = require('./db');
-
+const db = require("./db");
 
 // connect to db
 mongoose.connect(db.dbconf, db.config);
-
-
 
 const app = express();
 module.exports = app;
@@ -16,49 +13,55 @@ const bodyParser = require("body-parser"); // parse incoming HTTP POST data
 const multer = require("multer"); // handle HTTP POST with file uploads
 const axios = require("axios"); // making request to API
 const morgan = require("morgan"); // logging HTTP requests
-const cors = require('cors'); // allow Cross Site Requests
-const passport = require('passport'); // auth
+const cors = require("cors"); // allow Cross Site Requests
+const passport = require("passport"); // auth
 
 // Import constants
 const CONSTANTS = require("./_constants");
 
-app.use(cors({
-  origin: CONSTANTS.FRONTEND_URL
-}));
+app.use(
+  cors({
+    origin: CONSTANTS.FRONTEND_URL,
+  })
+);
 
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
 
 // configure the app to use bodyParser()
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.use(bodyParser.json());
 
+// use static folder 'static'
+app.use("/static", express.static("static"));
 
 // get config
 let secret;
 let port;
 let url;
-if (process.env.NODE_ENV === 'PRODUCTION') {
-	// if we're in PRODUCTION mode, then read the configration from a file
-	// use blocking file io to do this...
-	const fs = require('fs');
-	const path = require('path');
-	const fn = path.join(__dirname, '../config.json');
-	const data = fs.readFileSync(fn);
+if (process.env.NODE_ENV === "PRODUCTION") {
+  // if we're in PRODUCTION mode, then read the configration from a file
+  // use blocking file io to do this...
+  const fs = require("fs");
+  const path = require("path");
+  const fn = path.join(__dirname, "../config.json");
+  const data = fs.readFileSync(fn);
 
-	// our configuration file will be in json, so parse it and set the
-	// conenction string appropriately!
-	const conf = JSON.parse(data);
-	secret = conf.sessionSecret;
-	port = conf.port;
-	url = conf.url;
+  // our configuration file will be in json, so parse it and set the
+  // conenction string appropriately!
+  const conf = JSON.parse(data);
+  secret = conf.sessionSecret;
+  port = conf.port;
+  url = conf.url;
 } else {
-	// if we're not in PRODUCTION mode, then use
-	secret = '12345678910';
-	port = 3000;
-	url = 'http://localhost';
+  // if we're not in PRODUCTION mode, then use
+  secret = "12345678910";
+  port = 3000;
+  url = "http://localhost";
 }
 
 // Setup Passport
@@ -66,22 +69,22 @@ if (process.env.NODE_ENV === 'PRODUCTION') {
 app.use(passport.initialize());
 app.use(passport.session());
 
-const initPassport = require('./passport/init');
+const initPassport = require("./passport/init");
 initPassport(passport);
 
-
 // Routes
-const indexRoutes = require('./routes/index')
-const userRoutes = require('./routes/user')
-const authRoutes = require('./routes/auth');
-const signupRoutes = require('./routes/signup')
-const searchRoutes = require('./routes/search')
+const indexRoutes = require("./routes/index");
+const userRoutes = require("./routes/user");
+const authRoutes = require("./routes/auth");
+const signupRoutes = require("./routes/signup");
+const searchRoutes = require("./routes/search");
+const restaurantRoutes = require("./routes/restaurants");
 
-app.use('/', indexRoutes);
-app.use('/user', userRoutes);
-app.use('/auth', authRoutes);
-app.use('/signup', signupRoutes);
-app.use('/search', searchRoutes);
-
+app.use("/", indexRoutes);
+app.use("/user", userRoutes);
+app.use("/auth", authRoutes);
+app.use("/signup", signupRoutes);
+app.use("/search", searchRoutes);
+app.use("/restaurants", restaurantRoutes);
 
 module.exports = app;
--- back-end/routes/search.js
+++ back-end/routes/search.js
@@ -1,52 +1,46 @@
 const express = require("express");
-const router = require('express').router();
+const router = express.Router();
 const axios = require("axios");
-
 
 //mockaroo data object
 const searchData = () => {
-    try {
-        return axios.get('https://my.api.mockaroo.com/business.json?key=48c08110')
-    } catch (error) {
-        console.log(error);
-
-    }
-}
+  try {
+    return axios.get("https://my.api.mockaroo.com/business.json?key=48c08110");
+  } catch (error) {
+    console.log(error);
+  }
+};
 let searchResultArray = [];
-let searchResult = {searchResults: searchResultArray};
+let searchResult = { searchResults: searchResultArray };
 const category = searchData.category;
 
 //for fuzzy search
 function escapeRegex(text) {
-    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
-};
+  return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+}
 
-
-router.get("/search", function(req,res){
-    for (let i = 0; i<searchData.length; i++){
-        for (let j=0; j<searchData[i].category.length; j++){
-            if (searchData[i].category[j] === req.param.id){
-                if(! searchResultArray.include(searchData[i]) ){
-                    searchResultArray.push(searchData[i]);
-                }
-            }
+router.get("/search", function(req, res) {
+  for (let i = 0; i < searchData.length; i++) {
+    for (let j = 0; j < searchData[i].category.length; j++) {
+      if (searchData[i].category[j] === req.param.id) {
+        if (!searchResultArray.include(searchData[i])) {
+          searchResultArray.push(searchData[i]);
         }
+      }
     }
+  }
 
-    res.json(searchResult);
-    // if (req.query.search){
-    //     //get relevant search results
-    //     const regex = new RegExp(escapeRegex(req.query.search), 'gi');
-    //     // get results from db 
-    //     res.send()
+  res.json(searchResult);
+  // if (req.query.search){
+  //     //get relevant search results
+  //     const regex = new RegExp(escapeRegex(req.query.search), 'gi');
+  //     // get results from db
+  //     res.send()
 
+  // } else {
+  //     //get top restaurants around
 
-    // } else {
-    //     //get top restaurants around
-
-    // }
-
+  // }
 });
 
 module.exports = router;
-
