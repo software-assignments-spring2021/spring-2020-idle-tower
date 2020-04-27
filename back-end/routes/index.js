808ab4978336e4d0642cdc90ada2531e2538833f
Eric Fang <ericfang11@gmail.com>
2020-04-27 16:28:04 -0700

    List restaurants route

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,20 +1,20 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // GET index
-router.get('/', (req, res) => {
-	const context = {
-		lists: 'a',
-	}
-	// res.render('index', context);
-	res.set('Content-Type', 'text/html')
-	res.send('<p>some html</p>');
+router.get("/", (req, res) => {
+  const context = {
+    lists: "a",
+  };
+  // res.render('index', context);
+  res.set("Content-Type", "text/html");
+  res.send("<p>some html</p>");
 });
 
-router.get('/a', (req, res) => {
-	const lists = {'a': '1'};
+router.get("/a", (req, res) => {
+  const lists = { a: "1" };
 
-	res.json(lists);
+  res.json(lists);
 });
 
 module.exports = router;
---
+++ back-end/routes/restaurants.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const axios = require("axios");
+
+// GET route for restaurants
+router.get("/list", (req, res) => {
+  axios
+    .get("https://my.api.mockaroo.com/list_restaurants.json?key=552a1800")
+    .then((response) => {
+      res.send(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+module.exports = router;
