b02b6757cf4289339dc6cde77d5806c2933fc5dd
Jake Berkson <berksonj@gmail.com>
2020-04-29 13:55:37 -0400

    start setting up login page to interact w backend. TODO: save JWT token in local storage TODO: redirect to dashboard page on successful login

--- front-end/src/login.jsx
+++ front-end/src/login.jsx
@@ -1,28 +1,55 @@
-import React from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from "react-router-dom";
+import { UserContext } from './user-context.js';
+import { BACKEND_URL } from './_constants';
+import { postData } from './_helpers';
+
 import './login.scss';
 
 import Notification from './notification';
 
 const Login = (props) => {
-  const { notificationText, error } = props;
+  const [userState, setUserState] = useContext(UserContext);
+  const [notificationEl, setNotificationEl] = useState(<Notification text="Please login" error="" />);
+
+  const submitLogin = (event) => {
+    event.preventDefault();
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    const bodyData = { email: email, password: password };
+    postData(BACKEND_URL + '/auth/login', bodyData)
+      .then((res) => {
+        if (!res.error) {
+          console.log(res);
+          const newUserState = userState;
+          newUserState['logged-in'] = true;
+          setUserState(newUserState);
+
+          // TODO: save JWT token in local storage
+          // TODO: redirect to dashboard page
+        } else {
+          console.log("error!!");
+          setNotificationEl(<Notification text="Please login" error="Error loging in. Please try again" />);
+        }
+      });
+  }
 
   return (
     <div className="Login">
-      <Notification text={notificationText} error={error} />
+      <h2>Login</h2>
+      {notificationEl}
       <form action="" method="" acceptCharset="utf-8">
-        <label htmlFor="username">
+        <label htmlFor="email">
           Username:
-          {' '}
-          <input type="text" name="username" id="username" />
+          <input type="text" name="email" id="email" />
         </label>
         <label htmlFor="password">
           Password:
-          {' '}
           <input type="password" name="password" id="password" />
         </label>
-        <input type="submit" name="submit" value="Submit" />
+        <input type="submit" name="submit" value="Submit" onClick={submitLogin} />
       </form>
       <br/>
       <p>New user?</p>
@@ -31,16 +58,6 @@ const Login = (props) => {
       </Link>
     </div>
   );
-};
-
-Login.propTypes = {
-  notificationText: PropTypes.string,
-  error: PropTypes.string,
-};
-
-Login.defaultProps = {
-  notificationText: 'Welcome to FoodCheck! Please login',
-  error: '',
 };
 
 export default Login;
