328aaf1d16fd9c6f076f7bb59cde4e2fc2be425b
Jake Berkson <berksonj@gmail.com>
2020-04-30 11:05:02 -0400

    Remove useless config

--- front-end/src/_constants.js
+++ front-end/src/_constants.js
@@ -1,23 +1,3 @@
-// get config
-let url;
-if (process.env.NODE_ENV === "PRODUCTION") {
-  // if we're in PRODUCTION mode, then read the configration from a file
-  // use blocking file io to do this...
-  const fs = require("fs");
-  const path = require("path");
-  const fn = path.join(__dirname, "../../config.json");
-  const data = fs.readFileSync(fn);
-
-  // our configuration file will be in json, so parse it and set the
-  // conenction string appropriately!
-  const conf = JSON.parse(data);
-  url = conf.backend_url;
-} else {
-  // if we're not in PRODUCTION mode, then use
-  url = "http://localhost:3000";
-}
-
-
-const BACKEND_URL = url;
+const BACKEND_URL = "http://localhost:3000";
 
 export { BACKEND_URL };
