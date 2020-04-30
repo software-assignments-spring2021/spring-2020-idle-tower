2654fce09974d073bbf2d69c464b3adf37c39043
Jake Berkson <berksonj@gmail.com>
2020-04-30 11:33:01 -0400

    Update env var name and add sample env file

--- front-end/src/_constants.js
+++ front-end/src/_constants.js
@@ -1,7 +1,7 @@
 let BACKEND_URL;
 
 if (process.env.REACT_APP_URL) {
-	BACKEND_URL = process.env.REACT_APP_URL
+	BACKEND_URL = process.env.REACT_APP_BACKEND_URL
 } else {
 	BACKEND_URL = "http://localhost:3000";
 }
