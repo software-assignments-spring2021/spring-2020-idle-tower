c8bff7da156d52451d3b18c716833c9a60732db7
Jake Berkson <berksonj@gmail.com>
2020-04-29 22:05:40 -0400

    remove code that didn't work

--- front-end/src/sidebar.js
+++ front-end/src/sidebar.js
@@ -24,31 +24,6 @@ export default (props) => {
   let [signup_link, set_signup_link] = useState(start_signup_link);;
   let [logout_link, set_logout_link]= useState(start_logout_link);
 
-  useEffect(() => {
-    if (!userState['logged-in'] === false) {
-      set_login_link(
-        <Link to="/login" className="menu-item">
-          Login
-        </Link>
-      )
-      set_signup_link(
-        <Link to="/signup" className="menu-item">
-          Sign Up
-        </Link>
-      )
-      set_logout_link(null);
-    } else {
-      set_login_link(null);
-      set_signup_link(null);
-
-      set_logout_link(
-        <Link to="/logout" className="menu-item">
-          Logout
-        </Link>
-      )
-    }
-  }, [userState]);
-
   return (
     // Pass on our props
     <Menu {...props}>
