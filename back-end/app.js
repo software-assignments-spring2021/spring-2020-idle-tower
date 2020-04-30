df0734def39fa6e902da49bfabdc173cdac584a5
Jake Berkson <berksonj@gmail.com>
2020-04-30 11:00:49 -0400

    Remove unused config

--- back-end/app.js
+++ back-end/app.js
@@ -41,28 +41,6 @@ app.use(bodyParser.json());
 // use static folder 'static'
 app.use("/static", express.static("static"));
 
-// get config
-let port;
-let url;
-if (process.env.NODE_ENV === "PRODUCTION") {
-  // if we're in PRODUCTION mode, then read the configration from a file
-  // use blocking file io to do this...
-  const fs = require("fs");
-  const path = require("path");
-  const fn = path.join(__dirname, "../../config.json");
-  const data = fs.readFileSync(fn);
-
-  // our configuration file will be in json, so parse it and set the
-  // conenction string appropriately!
-  const conf = JSON.parse(data);
-  port = conf.backend_port;
-  url = conf.backend_url;
-} else {
-  // if we're not in PRODUCTION mode, then use
-  port = 3000;
-  url = "http://localhost";
-}
-
 // Routes
 const indexRoutes = require("./routes/index");
 const userRoutes = require("./routes/user");
