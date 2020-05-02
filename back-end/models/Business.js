d10fa430c50751ffd3d218e66bce3bc7888cc851
Melody Lam <yuetyumelody@gmail.com>
2020-05-02 17:52:16 -0400

    added city property in business schema, added mongoose connection for restaurants.js

--- back-end/models/Business.js
+++ back-end/models/Business.js
@@ -14,6 +14,10 @@ const BusinessSchema = new Schema({
         type: String,
         required: true,
     },
+    "city":{
+        type: String,
+        required: true,
+    },
     "stars":{
         type: Number,
         required: true,
--- back-end/routes/restaurants.js
+++ back-end/routes/restaurants.js
@@ -1,17 +1,31 @@
 const express = require("express");
 const router = express.Router();
-const axios = require("axios");
+// const axios = require("axios");
+const Business = require("../models/Business")
 
 // GET route for restaurants
 router.get("/list", (req, res) => {
-  axios
-    .get("https://my.api.mockaroo.com/list_restaurants.json?key=552a1800")
-    .then((response) => {
-      res.send(response.data);
+  Business.find(
+    { $or :
+    [{ city : "New York"},{dollar_sign : {$lt : 4}},]})
+    .then( list => {
+      res.json({
+        confirmation: 'success',
+        data: list
+      })
     })
-    .catch((error) => {
-      console.log(error);
-    });
+    .catch (err => {
+      confirmation: 'fail'
+    })
+
+  // axios
+  //   .get("https://my.api.mockaroo.com/list_restaurants.json?key=552a1800")
+  //   .then((response) => {
+  //     res.send(response.data);
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
+  //   });
 });
 
 module.exports = router;
